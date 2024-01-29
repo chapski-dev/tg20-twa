ca09d8980df28f29a7904eeda44e169f6e07b7ef
Alexey Chapski <alexey.chapski@gmail.com>
2024-01-29 14:55:11 +0300

    feat: Improve 'convertNumberToShortFormat' util.

--- src/utils/convertNumberToShortFormat.ts
+++ src/utils/convertNumberToShortFormat.ts
@@ -1,12 +1,16 @@
 export const convertNumberToShortFormat = (number: number): string => {
+  const absValue = Math.abs(Number(number))
+
   switch (true) {
-    case number < 1000:
-      return number.toString()
-    case number < 1000000:
-      return (number / 1000).toFixed(0) + 'K'
-    case number < 1000000000:
-      return (number / 1000000).toFixed(1) + 'M'
+    case absValue >= 1.0e12:
+      return (absValue / 1.0e12).toFixed(2).slice(0, -3) + 'MM'
+    case absValue >= 1.0e9:
+      return (absValue / 1.0e9).toFixed(2).slice(0, -3) + 'B'
+    case absValue >= 1.0e6:
+      return (absValue / 1.0e6).toFixed(2).slice(0, -3) + 'M'
+    case absValue >= 1.0e3:
+      return (absValue / 1.0e3).toFixed(2).slice(0, -3) + 'K'
     default:
-      return (number / 1000000000).toFixed(1) + 'B'
+      return absValue.toString()
   }
 }
