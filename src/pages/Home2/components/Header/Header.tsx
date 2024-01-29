4ba9f3e1c22e5228a85f5e8171becb2229c656d1
fiezzy <maks.zhers@gmail.com>
2024-01-29 20:35:30 +0300

    fix: hotfix

--- src/pages/Home2/components/Header/Header.tsx
+++ src/pages/Home2/components/Header/Header.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-import { SvgLoop, SvgNotification, SvgTg20 } from 'ui/icons';
+import { SvgLoop, SvgNotification, SvgTg20 } from 'ui/icons'
 
-import * as S from './style';
-
-import { Input } from 'ui/Input/Input';
-
+import { Input } from 'ui/Input/Input'
+import * as S from './style'
 
 export const Header = () => {
-    const [value, setValue] = useState('');
-    return (
-        <S.Flex>
-            <SvgTg20 className='tg-20' />
-            <Input className='search' icon={<SvgLoop />} onChange={(e) => setValue(e.target.value)} value={value} placeholder='Search tokens' />
-            <S.Notifications value={1}>
-                <SvgNotification />
-            </S.Notifications>
-        </S.Flex>
-    )
+  const [value, setValue] = useState('')
+  return (
+    <S.Flex>
+      <SvgTg20 className="tg-20" />
+      <Input
+        className="search"
+        icon={<SvgLoop />}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search tokens"
+        value={value}
+      />
+      <S.Notifications value={1}>
+        <SvgNotification />
+      </S.Notifications>
+    </S.Flex>
+  )
 }
