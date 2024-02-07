e9060b2c28d618809c045c7675b5b9503a3e8cfe
ch1cano <ch1canoo358@gmail.com>
2024-02-07 16:49:49 +0300

    feat/32: redesign Wallet page

--- src/pages/MyWallet/MyWallet.tsx
+++ src/pages/MyWallet/MyWallet.tsx
@@ -1,13 +1,34 @@
 import { FC, useMemo, useState } from 'react'
 import { useTonAddress } from '@tonconnect/ui-react'
 import { useNavigate } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 import { BackButton } from 'features/BackButton'
 import { useClipboard } from 'hooks/useClipboard/useClipboard'
+import { useTelegram } from 'hooks/useTelegram/useTelegram'
 import { Container } from 'ui/Container/Container'
-import { SvgAssetsIcon, SvgTransfersIcon } from 'ui/icons'
+import {
+  SvgArrowRightSlide,
+  SvgArrowSwap,
+  SvgAssetsIcon,
+  SvgIconWalletTg,
+  SvgLiSearch,
+  SvgLogout,
+  SvgRecieveSquare,
+  SvgSearch,
+  SvgSendSquare,
+  SvgSlideTitle,
+  SvgTransfersIcon,
+  SvgVectorsWallet,
+} from 'ui/icons'
 import { type Tab } from 'ui/TabsFilled/TabsFilled'
 import { shortenAddress } from 'utils/shortenAddress'
+import creditCards from './assets/credit-cards.png'
 import { MyAssets, MyTransfers, BalancesBlock } from './components'
+import { PROCENT_MOCK } from './mock'
 import * as S from './style'
 
 const tabs: Tab[] = [
@@ -27,6 +48,8 @@ export const MyWallet: FC = () => {
   const [currentTab, setCurrentTab] = useState(tabs[0])
 
   const userWalletAddress = useTonAddress()
+
+  const { currentGramBalance, currentWalletBalance, tonPrice } = useTelegram()
 
   const navigate = useNavigate()
 
@@ -51,10 +74,192 @@ export const MyWallet: FC = () => {
   }
 
   return (
-    <Container>
+    <S.Wrapper>
       <BackButton onClick={() => navigate(-1)} />
-      <S.Wrapper>
-        <S.BlockWrapper>
+      <S.TopWrapperBlock>
+        <S.TopBlock>
+          <S.Search>
+            <S.SearchInput
+              name="search"
+              placeholder="Search tokens"
+              // style={{
+              //   backgroundImage:
+              //     "url('../../pages/MyWallet/assets/li_search.png')",
+              //   backgroundPosition: 'left 5px',
+              //   backgroundSize: '20px 20px',
+              //   backgroundRepeat: 'no-repeat',
+              //   paddingLeft: '8px',
+              // }}
+              type="text"
+            />
+          </S.Search>
+          <S.LogOut
+            onClick={() => alert('Do you want to get out of your wallet?')}
+          >
+            <SvgLogout />
+          </S.LogOut>
+        </S.TopBlock>
+        <S.BalanceBlock>
+          <S.TotlaBalance>Total Balance</S.TotlaBalance>
+          <S.Balance>${currentWalletBalance}</S.Balance>
+          <S.InfoChange>
+            <S.Time>24h change</S.Time>
+            <S.Procent>{PROCENT_MOCK.procent}%</S.Procent>
+          </S.InfoChange>
+          <S.SvgRightDown>
+            <SvgIconWalletTg />
+          </S.SvgRightDown>
+        </S.BalanceBlock>
+      </S.TopWrapperBlock>
+
+      <S.WalletFunctions>
+        <S.SendBlockWrapper onClick={() => alert('Send button')}>
+          <S.SendButton>
+            <SvgSendSquare />
+          </S.SendButton>
+          <S.SendText>Send</S.SendText>
+        </S.SendBlockWrapper>
+        <S.RecieveBlockWrapper onClick={() => alert('Recieve button')}>
+          <S.RecieveButton>
+            <SvgRecieveSquare />
+          </S.RecieveButton>
+          <S.RecieveText>Recieve</S.RecieveText>
+        </S.RecieveBlockWrapper>
+        <S.SwapBlockWrapper onClick={() => alert('Swap button')}>
+          <S.SwapButton>
+            <SvgArrowSwap />
+          </S.SwapButton>
+          <S.SwapText>Swap</S.SwapText>
+        </S.SwapBlockWrapper>
+      </S.WalletFunctions>
+      <S.Line />
+      {/* <BalancesBlock /> */}
+
+      <S.CarouselContainer>
+        <Swiper
+          onSlideChange={() => {
+            console.log('slider change')
+          }}
+          slidesPerView={2}
+          spaceBetween={150}
+        >
+          <SwiperSlide>
+            <S.SwiperCard>
+              <S.SvgTitle>
+                <SvgSlideTitle />
+              </S.SvgTitle>
+              <S.ContentCard>
+                <S.TitleCard>Add Crypto from Binance or Coinbase</S.TitleCard>
+                <S.TextLink
+                  onClick={() => {
+                    alert('Deposit now')
+                  }}
+                >
+                  Deposit now
+                  <SvgArrowRightSlide />
+                </S.TextLink>
+              </S.ContentCard>
+              <S.Close
+                onClick={() => {
+                  alert('Close the card')
+                }}
+              >
+                X
+              </S.Close>
+            </S.SwiperCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <S.SwiperCardSecound>
+              <S.SvgTitleSecound>
+                <img alt="credit-cards-png" src={creditCards} width={90} />
+              </S.SvgTitleSecound>
+              <S.ContentCardSecound>
+                <S.TitleCardSecound>
+                  See what’s new in your wallet!
+                </S.TitleCardSecound>
+                <S.TextLinkSecound
+                  onClick={() => {
+                    alert('Explore wallet')
+                  }}
+                >
+                  Explore wallet
+                  <SvgArrowRightSlide />
+                </S.TextLinkSecound>
+              </S.ContentCardSecound>
+              <S.Close
+                onClick={() => {
+                  alert('Close the card')
+                }}
+              >
+                X
+              </S.Close>
+            </S.SwiperCardSecound>
+          </SwiperSlide>
+          <SwiperSlide>
+            <S.SwiperCard>
+              <S.SvgTitle>
+                <SvgSlideTitle />
+              </S.SvgTitle>
+              <S.ContentCard>
+                <S.TitleCard>Add Crypto from Binance or Coinbase</S.TitleCard>
+                <S.TextLink
+                  onClick={() => {
+                    alert('Deposit now')
+                  }}
+                >
+                  Deposit now
+                  <SvgArrowRightSlide />
+                </S.TextLink>
+              </S.ContentCard>
+              <S.Close
+                onClick={() => {
+                  alert('Close the card')
+                }}
+              >
+                X
+              </S.Close>
+            </S.SwiperCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <S.SwiperCardSecound>
+              <S.SvgTitleSecound>
+                <img alt="credit-cards-png" src={creditCards} width={90} />
+              </S.SvgTitleSecound>
+              <S.ContentCardSecound>
+                <S.TitleCardSecound>
+                  See what’s new in your wallet!
+                </S.TitleCardSecound>
+                <S.TextLinkSecound
+                  onClick={() => {
+                    alert('Explore wallet')
+                  }}
+                >
+                  Explore wallet
+                  <SvgArrowRightSlide />
+                </S.TextLinkSecound>
+              </S.ContentCardSecound>
+              <S.Close
+                onClick={() => {
+                  alert('Close the card')
+                }}
+              >
+                X
+              </S.Close>
+            </S.SwiperCardSecound>
+          </SwiperSlide>
+        </Swiper>
+      </S.CarouselContainer>
+
+      <Container>
+        <S.TabsBlock>
+          <S.CustomTab
+            onChange={setCurrentTab}
+            selectedTab={currentTab}
+            tabs={tabs}
+          ></S.CustomTab>
+          {currentWalletContent}
+        </S.TabsBlock>
+        {/* <S.BlockWrapper>
           <BalancesBlock />
           <S.WalletAddressWrapper onClick={handleWalletAddressClick}>
             <S.WalletLabel>{shortenAddress(userWalletAddress)}</S.WalletLabel>
@@ -70,22 +275,23 @@ export const MyWallet: FC = () => {
             tabs={tabs}
           />
           {currentWalletContent}
-        </S.BlockWrapper>
-      </S.Wrapper>
-      {/* <MainButton
-        onClick={() => {
-          // navigate(AppRoutes.Inscribe)
-          navigate({
-            pathname: AppRoutes.Inscribe,
-            search: createSearchParams({
-              type: 'mint',
-              tick: 'gram',
-              from: 'token',
-            }).toString(),
-          })
-        }}
-        text="Inscribe"
-      /> */}
-    </Container>
+        </S.BlockWrapper> */}
+      </Container>
+    </S.Wrapper>
   )
 }
+
+/* <MainButton
+      onClick={() => {
+        // navigate(AppRoutes.Inscribe)
+        navigate({
+          pathname: AppRoutes.Inscribe,
+          search: createSearchParams({
+            type: 'mint',
+            tick: 'gram',
+            from: 'token',
+          }).toString(),
+        })
+      }}
+      text="Inscribe"
+      /> */
--- src/pages/MyWallet/style.ts
+++ src/pages/MyWallet/style.ts
@@ -1,36 +1,232 @@
 import styled from 'styled-components'
 import { SvgCopy } from 'ui/icons'
-import { TabsFilled as UITabs } from 'ui/TabsFilled/TabsFilled'
+import { TabsFilled } from 'ui/TabsFilled/TabsFilled'
+import { theme } from './../../assets/style/theme'
+// import bgVectors from './assets/Vector.png'
 
 export const Wrapper = styled.div`
   display: flex;
   min-width: 100%;
-  gap: 24px;
   flex-direction: column;
   align-self: center;
-  padding: 24px 0;
+  padding: 0px;
 `
 
-export const Tabs = styled(UITabs)`
-  min-width: 100%;
-`
-
-export const WalletAddressWrapper = styled.div`
+export const TopWrapperBlock = styled.div`
   display: flex;
-  align-items: center;
-  gap: 6px;
-  cursor: pointer;
-  margin: 0 auto;
+  height: 100%;
+  flex-direction: column;
 `
 
-export const CopyIcon = styled(SvgCopy)`
-  width: 16px;
-  height: 16px;
+export const TopBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+  padding-top: 16px;
+  padding-bottom: 10px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+  background-image: url('../../pages/MyWallet/assets/li_search.png');
+  background-repeat: no-repeat;
+`
 
-  path {
-    fill: ${({ theme }) => theme.color.link};
+export const Search = styled.form`
+  display: flex;
+`
+
+export const SearchInput = styled.input`
+  width: 310.68px;
+  height: 42px;
+  border: none;
+  border-radius: 6px;
+  color: ${({ theme }) => theme.color.text};
+  background-color: ${({ theme }) => theme.color.bg};
+  &::placeholder {
+    color: ${({ theme }) => theme.color.hint};
   }
 `
+
+export const LogOut = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 42px;
+  height: 42px;
+  background-color: rgba(233, 70, 70, 0.15);
+  border-radius: 6px;
+  cursor: pointer;
+`
+
+export const BalanceBlock = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  padding-top: 23.5px;
+  padding-bottom: 23.4px;
+  padding-left: 20px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+`
+export const TotlaBalance = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  color: ${({ theme }) => theme.color.hint};
+`
+
+export const Balance = styled.span`
+  font-size: 30px;
+  line-height: 36.31px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.text};
+  padding-top: 16px;
+`
+
+export const InfoChange = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding-top: 2px;
+`
+
+export const Time = styled.span`
+  font-size: 12px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.hint};
+`
+
+export const Procent = styled.span`
+  font-size: 12px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.greenSuccess};
+`
+
+export const SvgRightDown = styled.div`
+  position: absolute;
+  right: 12.32px;
+  bottom: 12.32px;
+
+  path {
+    fill: ${({ theme }) => theme.color.text};
+  }
+`
+
+export const WalletFunctions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.color.text};
+  gap: 30px;
+  font-size: 14px;
+  font-weight: 500;
+  padding-top: 24px;
+  padding-bottom: 24px;
+`
+
+export const SendBlockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 7px;
+  cursor: pointer;
+`
+
+export const SendButton = styled.button`
+  width: 52.66px;
+  height: 52.66px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    path {
+      stroke: ${({ theme }) => theme.color.text};
+    }
+  }
+`
+
+export const SendText = styled.span`
+  font-size: 14.18px;
+  font-weight: 500;
+`
+
+export const RecieveBlockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 7px;
+  cursor: pointer;
+`
+
+export const RecieveButton = styled.button`
+  width: 52.66px;
+  height: 52.66px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    path {
+      stroke: ${({ theme }) => theme.color.text};
+    }
+  }
+`
+
+export const RecieveText = styled.span`
+  font-size: 14.18px;
+  font-weight: 500;
+`
+
+export const SwapBlockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 7px;
+  cursor: pointer;
+`
+
+export const SwapButton = styled.button`
+  width: 52.66px;
+  height: 52.66px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    path {
+      stroke: ${({ theme }) => theme.color.text};
+    }
+  }
+`
+
+export const SwapText = styled.span`
+  font-size: 14.18px;
+  font-weight: 500;
+`
+
+export const Line = styled.div`
+  border: 3px solid ${({ theme }) => theme.color.bgSecondary};
+`
+
+export const CustomTab = styled(TabsFilled)`
+  svg {
+    display: none;
+  }
+  color: red;
+`
+
+// export const Tabs = styled(UITabs)`
+//   min-width: 100%;
+// `
+
+// export const WalletAddressWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   gap: 6px;
+//   cursor: pointer;
+//   margin: 0 auto;
+// `
 
 export const WalletLabel = styled.span`
   font-size: 14px;
@@ -42,10 +238,107 @@ export const WalletLabel = styled.span`
   word-break: break-all;
 `
 
-export const BlockWrapper = styled.div`
+export const TabsBlock = styled.div`
   display: flex;
-  align-items: center;
   flex-direction: column;
   width: 100%;
   gap: 12px;
+`
+
+export const CarouselContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  padding-left: 15px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`
+
+export const SwiperCard = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  gap: 16.22px;
+  width: 257px;
+  height: 75px;
+  background-color: rgba(255, 217, 75, 0.18);
+  color: ${({ theme }) => theme.color.text};
+  border-radius: 6px;
+  padding: 8px;
+  cursor: grab;
+`
+
+export const SvgTitle = styled.div``
+
+export const ContentCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 7.5px;
+`
+
+export const TitleCard = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`
+
+export const TextLink = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  font-weight: 500;
+  cursor: pointer;
+  color: rgba(246, 154, 47, 1); // add constant color
+`
+
+export const Close = styled.div`
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  font-size: 10px;
+  cursor: pointer;
+`
+
+export const SwiperCardSecound = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  gap: 16.22px;
+  width: 257px;
+  height: 75px;
+  background-color: rgba(214, 183, 255, 0.2);
+  color: ${({ theme }) => theme.color.text};
+  border-radius: 6px;
+  padding: 8px;
+  cursor: grab;
+`
+
+export const SvgTitleSecound = styled.div``
+
+export const ContentCardSecound = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  margin-left: -20px;
+  gap: 7.5px;
+`
+
+export const TitleCardSecound = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`
+
+export const TextLinkSecound = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  font-weight: 500;
+  cursor: pointer;
+  color: rgba(164, 95, 255, 1); // add constant color
+  svg {
+    path {
+      fill: rgba(164, 95, 255, 1);
+    }
+  }
 `
