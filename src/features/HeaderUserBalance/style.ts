7f8973c721074eadd511494e2cdf97644442738c
Alexey Chapski <alexey.chapski@gmail.com>
2024-02-08 15:46:07 +0300

    style(HeaderUserBalance): Change bg color.

--- src/features/HeaderUserBalance/style.ts
+++ src/features/HeaderUserBalance/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Text as UIText} from 'ui';
-import {  SvgLogo2 as UISvgLogo2, SvgWallet as UISvgWallet } from 'ui/icons';
+import { SvgWallet as UISvgWallet } from 'ui/icons';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
   align-items: center;
   gap: 10px;
   padding: 5px 8px;
-  background-color: ${({theme}) => theme.color.bgSecondary}
+  background-color: ${({theme}) => theme.color.bg}
 `;
 
 export const Text = styled(UIText)`
