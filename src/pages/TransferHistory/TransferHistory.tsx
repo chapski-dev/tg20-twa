97fec55f0dc06853e86ff379859ba36d179bb357
ch1cano <ch1canoo358@gmail.com>
2024-02-12 16:50:10 +0300

    feat/55: add TransferHistort page

--- src/pages/TransferHistory/TransferHistory.tsx
+++ src/pages/TransferHistory/TransferHistory.tsx
@@ -1,10 +1,48 @@
-import { FC } from 'react'
+import { FC, ReactHTMLElement } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { AppRoutes } from 'constants/app'
 import { BackButton } from 'features/BackButton'
+import {
+  SvgArrowSwap,
+  SvgLogoHistoryToken,
+  SvgRecieveSquare,
+  SvgSendSquare,
+  SvgTrade,
+} from 'ui/icons'
 import * as S from './style'
 
-export const TransferHistory: FC = () => {
+type FunctionalProps = {
+  title: string
+  icon: JSX.Element
+  action: () => void
+}
+
+const functionalBlocks: FunctionalProps[] = [
+  {
+    title: 'Send',
+    icon: <SvgSendSquare />,
+    action: () => alert('Send button'),
+  },
+  {
+    title: 'Recieve',
+    icon: <SvgRecieveSquare />,
+    action: () => alert('Recieve button'),
+  },
+  { title: 'Swap', icon: <SvgArrowSwap />, action: () => alert('Swap button') },
+  { title: 'Trade', icon: <SvgTrade />, action: () => alert('Trade button') },
+]
+
+const TRANSFER_MOCK = {
+  balance: '280 000 000 GRAM',
+  dollarCount: '800.28',
+  supply: '210B',
+  price: '0.001',
+  volume: '2.6M',
+}
+
+export const TransferHistory: FC = (props) => {
+  // const { balance, dollarCount, supply, price, volume } = props
+
   const navigate = useNavigate()
 
   const { tick } = useParams()
@@ -14,7 +52,45 @@ export const TransferHistory: FC = () => {
   return (
     <S.Wrapper>
       <BackButton onClick={() => navigate(AppRoutes.MyWallet)} />
-      TransferHistory
+      <S.BalanceBlock>
+        <S.TopBlock>
+          <S.Logo>
+            <S.BackGroundSvg>
+              <SvgLogoHistoryToken />
+            </S.BackGroundSvg>
+          </S.Logo>
+          <S.Balance>{TRANSFER_MOCK.balance}</S.Balance>
+          <S.DollarCount>~ $ {TRANSFER_MOCK.dollarCount}</S.DollarCount>
+        </S.TopBlock>
+        <S.FunctionalBlock>
+          {functionalBlocks.map(({ icon, title, action }, idx) => (
+            <S.BlockWrapper key={idx} onClick={action}>
+              <S.Button>{icon}</S.Button>
+              <S.Text>{title}</S.Text>
+            </S.BlockWrapper>
+          ))}
+        </S.FunctionalBlock>
+      </S.BalanceBlock>
+      <S.InfoTotal>
+        <S.Item>
+          <S.Title>Total Supply</S.Title>
+          <S.Count>{TRANSFER_MOCK.supply}</S.Count>
+        </S.Item>
+        <S.Item>
+          <S.Title>Floor Price</S.Title>
+          <S.Count>${TRANSFER_MOCK.price}</S.Count>
+        </S.Item>
+        <S.Item>
+          <S.Title>Total Volume</S.Title>
+          <S.Count>{TRANSFER_MOCK.volume}</S.Count>
+        </S.Item>
+      </S.InfoTotal>
+      <S.Line />
+      <S.BtnBlock>
+        <S.CheckExplorer>
+          Can’t find your transaction?<span> Check explorer</span>
+        </S.CheckExplorer>
+      </S.BtnBlock>
     </S.Wrapper>
   )
 }
--- src/pages/TransferHistory/style.ts
+++ src/pages/TransferHistory/style.ts
@@ -2,4 +2,148 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+export const BalanceBlock = styled.div`
+  width: 100%;
+  height: 280px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+  padding-bottom: 24px;
+`
+
+export const TopBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 24px;
+  gap: 8px;
+`
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const BackGroundSvg = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  background-color: rgba(0, 122, 255, 1);
+`
+
+export const Balance = styled.span`
+  font-size: 24px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.text};
+`
+
+export const DollarCount = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.hint};
+`
+
+export const FunctionalBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 24px;
+  gap: 24px;
+`
+
+export const BlockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 7px;
+  cursor: pointer;
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 52.66px;
+  height: 52.66px;
+  background-color: ${({ theme }) => theme.color.bg};
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    color: ${({ theme }) => theme.color.text};
+  }
+`
+
+export const Text = styled.span`
+  color: ${({ theme }) => theme.color.text};
+  font-size: 14.18px;
+  font-weight: 500;
+`
+
+export const InfoTotal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 24px;
+
+  :last-child {
+    align-items: flex-end;
+  }
+`
+
+export const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 20px;
+  color: ${({ theme }) => theme.color.hint};
+`
+
+export const Count = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  color: ${({ theme }) => theme.color.text};
+`
+
+export const Line = styled.div`
+  width: 100%;
+  height: 3px;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+`
+
+export const BtnBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`
+export const CheckExplorer = styled.button`
+  width: 333px;
+  height: 32px;
+  border: 1px dotted ${({ theme }) => theme.color.hint};
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.color.bg};
+  color: ${({ theme }) => theme.color.hint};
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 12px;
+
+  :last-child {
+    color: ${({ theme }) => theme.color.link};
+  }
 `
