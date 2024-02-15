e4b71fc9b53b760c9bdacfc72c464b1f63b0afa2
ch1cano <ch1canoo358@gmail.com>
2024-02-15 17:08:24 +0300

    feat/84: fix btn

--- src/pages/MyWallet/components/ReceivePopup/ReceivePopup.tsx
+++ src/pages/MyWallet/components/ReceivePopup/ReceivePopup.tsx
@@ -1,10 +1,8 @@
 import { FC } from 'react'
 import { useTonAddress } from '@tonconnect/ui-react'
 import { QRCodeSVG } from 'qrcode.react'
-import { theme } from 'assets/style/theme'
-import { MainButton } from 'features/MainButton'
 import { useClipboard } from 'hooks/useClipboard/useClipboard'
-import { Modal } from 'ui'
+import { Button, Modal } from 'ui'
 import * as S from './style'
 
 type ReceivePopupProps = {
@@ -30,11 +28,7 @@ export const ReceivePopup: FC<ReceivePopupProps> = ({ onClose }) => {
           <QRCodeSVG value={userWalletAddress} />
         </S.QrCodeWrapper>
         <S.CopyBtn>
-          <MainButton
-            color={theme.color.greenSuccess}
-            onClick={copyWalletAddress}
-            text="Copy"
-          />
+          <Button children="Copy" onClick={copyWalletAddress} />
         </S.CopyBtn>
       </S.WrapperModal>
     </Modal>
